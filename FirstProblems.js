32419773bb548a2838e2b957814214c2e0b02861
Cullen REDDY <cull.redd@gmail.com>
2022-03-09 10:45:32 -0800

    solve Euler prob 6

--- FirstProblems.js
+++ FirstProblems.js
@@ -103,12 +103,37 @@ What is the smallest positive number that is evenly divisible by all of the numb
 232792560 ; 494665th person
 */
 
-const arr = [];
-let i = 1;
-while (i < Infinity) {
-    if (i % 1 === 0 && i % 2 === 0 && i % 3 === 0 && i % 4 === 0 && i % 5 === 0 && i % 6 === 0 && i % 7 === 0 && i % 8 === 0 && i % 9 === 0 && i % 10 === 0 && i % 11 === 0 && i % 12 === 0 && i % 13 === 0 && i % 14 === 0 && i % 15 === 0 && i % 16 === 0 && i % 17 === 0 && i % 18 === 0 && i % 19 === 0 && i % 20 === 0) {
-        arr.push(i);
-        break;
-    }
-    i = i + 1;
+// const arr = [];
+// let i = 1;
+// while (i < Infinity) {
+//     if (i % 1 === 0 && i % 2 === 0 && i % 3 === 0 && i % 4 === 0 && i % 5 === 0 && i % 6 === 0 && i % 7 === 0 && i % 8 === 0 && i % 9 === 0 && i % 10 === 0 && i % 11 === 0 && i % 12 === 0 && i % 13 === 0 && i % 14 === 0 && i % 15 === 0 && i % 16 === 0 && i % 17 === 0 && i % 18 === 0 && i % 19 === 0 && i % 20 === 0) {
+//         arr.push(i);
+//         break;
+//     }
+//     i = i + 1;
+// }
+
+
+/** Problem 6) The sum of the squares of the first ten natural numbers is,
+"1 squared plus 2 squared plus ... ten squared equals 285"
+
+The square of the sum of the first ten natural numbers is,
+(1+2+...+10)**2 = 55**2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+3025-385-2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. 
+25164150, 497669th person , difficulty 5%
+*/
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i**2;
 }
+let sum2 = 0;
+for (let i = 1; i <= 100; i++) {
+    sum2 += i; 
+}
+square = sum**2;
+let diff = square - sum;
